4470b43533b6c40293cab3954093439f03a83ce0
ggastongastaldi-source <gastornet@gmail.com>
2026-04-16 17:21:06 -0300

    fix: conecta socketHandlers al io de server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const cors = require('cors');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: '*' } });
+require('./services/socketHandlers')(io);
 
 app.use(cors());
 app.use(express.json());
