cc9e4b023142ffb72e5f5b5ae7d51e6b9533e261
ggastongastaldi-source <ggastonnet@gmail.com>
2026-07-18 04:26:23 -0300

    revert: eliminar redirect www que causaba loop

--- server.js
+++ server.js
@@ -28,13 +28,6 @@ require('./src/core/services/mensajeriaSocket')(io);
 
 app.use(cors());
 
-// Redirigir www a sin-www
-app.use((req, res, next) => {
-  if (req.headers.host && req.headers.host.startsWith('www.')) {
-    return res.redirect(301, 'https://' + req.headers.host.slice(4) + req.url);
-  }
-  next();
-});
 
 // Permissions-Policy: habilitar microfono y camara para la PWA
 app.use((req, res, next) => {
